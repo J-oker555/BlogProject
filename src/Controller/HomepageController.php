0990b8e9f7fa46ae0a04a5f91f174e97d00a571d
Maalal Ilyas <maalal.ilyas@gmail.com>
2023-11-26 02:23:39 +0100

    Initial commit

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+    public function index(CategorieRepository $categorieRepository): Response
+
+    {
+        return $this->render('homepage/index.html.twig', [
+            'lastCategories' => $categorieRepository->findBy([], ['id' => 'DESC'], 3)
+        ]);
+    }
+}
